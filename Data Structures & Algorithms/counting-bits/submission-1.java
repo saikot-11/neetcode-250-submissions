b47f619b8f70376e95942256e2647bca4716e82e
Saikot Majumdar <smajumdar11@outlook.com>
2026-08-31 15:16:20 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int [] dp = new int [n + 1];
+        int offset = 1;
+
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (2 * offset == i) {
+                offset = i;
+            }
+            dp[i] = dp[i - offset] + 1;
+        }
+
+        return dp;
+    }
+}
