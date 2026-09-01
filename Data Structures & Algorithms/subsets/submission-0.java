d791e7eb713dda71a3abd5f13e758b85f7fdb78e
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-02 01:33:36 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+
+        for (int i = 0; i < Math.pow(2, len); i++) {
+            List<Integer> subset = new ArrayList<Integer>();
+            for (int j = 0; j < 32; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            res.add(subset);
+        }
+
+        return res;
+    }
+}
