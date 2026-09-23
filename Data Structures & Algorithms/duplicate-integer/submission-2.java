495b7dc218858206cb010f9f0c1811400016bb4d
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-23 19:54:19 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+    * Time Complexity: O(n)
+    * Space Complexity: O(n)
+    */
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
