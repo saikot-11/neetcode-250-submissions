0a9d25926bec002b793454dbe5a0e23719f05c0d
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-24 01:30:30 +0530

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1<<i)) != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
