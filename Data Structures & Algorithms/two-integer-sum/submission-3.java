1f07ae0809b3372c8da5edcd1aac87606a42f144
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-23 21:30:17 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+    * Time Complexity: O(n)
+    * Space Complexity: O(n)
+    */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (indexMap.containsKey(diff)) {
+                return new int [] {indexMap.get(diff), i};
+            }
+            indexMap.put(nums[i], i);
+        }
+
+        return new int [0];
+    }
+}
