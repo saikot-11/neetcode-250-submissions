47cf187c98f077a1e1e38814afb8e0560928268d
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-25 00:58:39 +0530

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    public int[] countBits(int n) {
+        int [] res = new int [n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int count = 0;
+            for (int j = 0; j < 32; j++) {
+                if ((i & (1 << j)) != 0) {
+                    count++;
+                }
+            }
+            res[i] = count;
+        }
+
+        return res;
+    }
+}
