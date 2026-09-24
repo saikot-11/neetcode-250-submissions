c654b349bc9dd7db5e025758a5eab2faeb66e8ac
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-25 01:06:02 +0530

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     */
+    public int reverseBits(int n) {
+        StringBuilder sb = new StringBuilder();
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                sb.append("1");
+            } else {
+                sb.append("0");
+            }
+        }
+
+        for (int i = 0; i < 32; i++) {
+            if (sb.charAt(i) == '1') {
+                res = res | (1 << (31 - i));
+            }
+        }
+
+        return res;
+    }
+}
