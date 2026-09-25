d39f626f055eed291775b58e6a511786fc1399d7
Saikot Majumdar <smajumdar11@outlook.com>
2026-09-25 15:18:58 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,19 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+
+        int [] dp = new int [cost.length + 1];
+
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], cost[i - 2] + dp[i - 2]);
+        }
+
+        return dp[cost.length];
+    }
+}
